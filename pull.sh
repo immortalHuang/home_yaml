8c00da4e29eb5739fb5bf8112c3b477fd195c652
veric.windows <1183768063@qq.com>
2017-09-23 13:11:26 +0800

    aaa

--- pull.sh
+++ pull.sh
@@ -1,16 +1,16 @@
-images=(                                                                                              
+images=(
 etcd-amd64:3.0.17
 pause-amd64:3.0
-k8s-dns-sidecar-amd64:1.14.1
-k8s-dns-kube-dns-amd64:1.14.1
-k8s-dns-dnsmasq-nanny-amd64:1.14.1
-)                                                                                                     
-                                                                                                      
+k8s-dns-sidecar-amd64:1.14.4
+k8s-dns-kube-dns-amd64:1.14.4
+k8s-dns-dnsmasq-nanny-amd64:1.14.4
+)
+
 for imageName in ${images[@]} ; do
     a=$imageName
     aa=`echo $a | cut -d ":" -f 1`
-    echo $aa                                                             
-    docker pull gcr.mritd.me/$imageName
-    docker tag gcr.mritd.me/$imageName gcr.io/google_containers/$imageName
-    docker rmi gcr.mritd.me/$imageName
-done                                                                                                  
+    echo $aa
+    docker pull gcr.mritd.me/google_containers/$imageName
+    docker tag gcr.mritd.me/google_containers/$imageName gcr.io/google_containers/$imageName
+    docker rmi gcr.mritd.me/google_containers/$imageName
+done
--- pull2.sh
+++ pull2.sh
@@ -1,12 +1,12 @@
-images=(                                                                                              
-kube-apiserver-amd64:v1.6.4
-kube-controller-manager-amd64:v1.6.4
-kube-scheduler-amd64:v1.6.4
-kube-proxy-amd64:v1.6.4
-)                                                                                                     
-                                                                                                      
-for imageName in ${images[@]} ; do                                                                    
-    docker pull immortalhuang/$imageName                                                   
-    docker tag docker.io/immortalhuang/$imageName gcr.io/google_containers/$imageName
-    docker rmi docker.io/immortalhuang/$imageName
-done                                                                                                  
+images=(
+kube-apiserver-amd64:v1.7.6
+kube-controller-manager-amd64:v1.7.6
+kube-scheduler-amd64:v1.7.6
+kube-proxy-amd64:v1.7.6
+)
+
+for imageName in ${images[@]} ; do
+    docker pull gcr.mritd.me/google_containers/$imageName
+    docker tag gcr.mritd.me/google_containers/$imageName gcr.io/google_containers/$imageName
+    docker rmi gcr.mritd.me/google_containers/$imageName
+done
